8315ed7137ce5d81fbeab2c57a69058bff32465e
Emin Fedar <eminfedar@gmail.com>
2020-06-12 14:27:38 +0300

    [ADDED] UDPSocket.Connect() & UDPSocket.Send() (faster than SendTo)

--- easysocket/include/udpsocket.h
+++ easysocket/include/udpsocket.h
@@ -16,13 +16,20 @@ public:
     std::function<void(std::string, std::string, std::uint16_t)> onMessageReceived;
     std::function<void(const char*, int, std::string, std::uint16_t)> onRawMessageReceived;
 
-    explicit UDPSocket(FDR_ON_ERROR, int socketId = -1);
+    explicit UDPSocket(bool useConnect = false, FDR_ON_ERROR, int socketId = -1);
 
     void SendTo(std::string message, std::string host, uint16_t port, FDR_ON_ERROR);
     void SendTo(const char *bytes, size_t byteslength, std::string host, uint16_t port, FDR_ON_ERROR);
 
+    int Send(std::string message);
+    int Send(const char *bytes, size_t byteslength);
+
+    void Connect(std::string host, uint16_t port, FDR_ON_ERROR);
+    void Connect(uint32_t ipv4, uint16_t port, FDR_ON_ERROR);
+
 private:
     static void Receive(UDPSocket *udpSocket);
+    static void ReceiveFrom(UDPSocket *udpSocket);
 };
 
 #endif
