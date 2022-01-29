a59eb494c73c3b7f676c3e3ef11e7c6d47b36d0a
Friedrich Engel <friedrichengel@mailbox.org>
2022-01-29 16:20:50 +0100

    added bind_broadcast for UDP_Server

--- async-sockets/include/udpserver.hpp
+++ async-sockets/include/udpserver.hpp
@@ -23,8 +23,39 @@ public:
             return;
         }
     }
+    
     void Bind(int port, FDR_ON_ERROR)
     {
         this->Bind("0.0.0.0", port, onError);
+    }
+
+    void Bind_Broadcast(std::string IPv4, std::uint16_t port, FDR_ON_ERROR)
+    {
+        if (inet_pton(AF_INET, IPv4.c_str(), &this->address.sin_addr) <= 0)
+        {
+            onError(errno, "Invalid address. Address type not supported.");
+            return;
+        }
+
+        this->address.sin_family = AF_INET;
+        this->address.sin_port = htons(port);
+
+        if (bind(this->sock, (const sockaddr*)&this->address, sizeof(this->address)) < 0)
+        {
+            onError(errno, "Cannot bind the socket.");
+            return;
+        }
+
+        int broadcast = 1;
+        if (setsockopt(this->sock, SOL_SOCKET, SO_BROADCAST, &broadcast, sizeof broadcast))
+        {
+            onError(errno, "setsockopt(SO_BROADCAST) failed.");
+            return;
+        }
+    }
+
+    void Bind_Broadcast(std::uint16_t port, FDR_ON_ERROR)
+    {
+        this->Bind_Broadcast("0.0.0.0", port, onError);
     }
 };
